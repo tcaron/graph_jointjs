5afe1306c3d22676e9ca7fdcbbf294a9e839ce1c
Eric RAMAT <eric.ramat@gmail.com>
2017-03-16 22:06:20 +0100

    Add french translations

--- app/views/footer/footer.js
+++ app/views/footer/footer.js
@@ -2,16 +2,25 @@
 
 angular.module('artisStudio.footer', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/footer', {
-    templateUrl: 'footer/footer.html',
-    controller: 'FooterCtrl',
-    access: {
-      requiredLogin: true
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/footer', {
+      templateUrl: 'footer/footer.html',
+      controller: 'FooterCtrl',
+      access: {
+        requiredLogin: true
+      }
+    });
+  }])
+
+  .controller('FooterCtrl', ['$scope', '$rootScope', '$location', 'gettextCatalog', 'tmhDynamicLocale', 'localStorageService',
+    function ($scope, $rootScope, $location, gettextCatalog, tmhDynamicLocale, localStorageService) {
+      $rootScope.local = gettextCatalog.getCurrentLanguage();
+      $scope.setLocale = function (local) {
+        gettextCatalog.setCurrentLanguage(local);
+        $scope.currentLanguage = local;
+        $rootScope.local = local;
+        localStorageService.set("local", local);
+        tmhDynamicLocale.set(local);
+      };
     }
-  });
-}])
-
-.controller('FooterCtrl', [function() {
-
-}]);
+  ]);
