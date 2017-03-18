f46d12f2340a58039be07c5c197a1abe7c58c459
Eric RAMAT <eric.ramat@gmail.com>
2017-03-18 08:18:14 +0100

    Add workspace and project creation

--- app/views/projects/projects.js
+++ app/views/projects/projects.js
@@ -2,29 +2,63 @@
 
 angular.module('artisStudio.projects', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/projects/:id/:name', {
-    templateUrl: 'views/projects/projects.html',
-    controller: 'ProjectsCtrl',
-    access: {
-      requiredLogin: true
-    }
-  });
-}])
-
-.controller('ProjectsCtrl', ['$scope', '$rootScope', '$routeParams', 'DataFactory', '$location',
-  function($scope, $rootScope, $routeParams, DataFactory, $location) {
-    var workspace_id = $routeParams.id;
-    var workspace_name = $routeParams.name;
-
-    $scope.projects = [];
-    $scope.workspace = workspace_name;
-    DataFactory.getData('projects/' + workspace_id).then(function (data) {
-      $scope.workspace = data.data.workspace;
-      $scope.projects = data.data.projects;
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/projects/:id/:name', {
+      templateUrl: 'views/projects/projects.html',
+      controller: 'ProjectsCtrl',
+      access: {
+        requiredLogin: true
+      }
     });
+  }])
 
-    $scope.models = function (project_id, project_name) {
-      $location.path("/models/" + project_id + "/" + project_name);
+  .controller('ProjectsCtrl', ['$scope', '$rootScope', '$routeParams', 'DataFactory', '$location', 'ModalService', '$http', 'Notification',
+    function ($scope, $rootScope, $routeParams, DataFactory, $location, ModalService, $http, Notification) {
+      var workspace_id = $routeParams.id;
+      var workspace_name = $routeParams.name;
+
+      $scope.projects = [];
+      $scope.workspace = workspace_name;
+      DataFactory.getData('projects/' + workspace_id).then(function (data) {
+        $scope.workspace = data.data.workspace;
+        $scope.projects = data.data.projects;
+      });
+
+      $scope.create = function () {
+        ModalService.showModal({
+          templateUrl: 'createProjectModal.html',
+          controller: "CreateProjectModalController"
+        }).then(function (modal) {
+          modal.element.modal();
+          modal.close.then(function (result) {
+            if (result.result) {
+              $http.post($rootScope.urlBase + '/api/v1/project/' + workspace_id, {
+                name: result.name
+              }).success(function (data) {
+                DataFactory.getData('projects/' + workspace_id).then(function (data) {
+                  $scope.workspace = data.data.workspace;
+                  $scope.projects = data.data.projects;
+                });
+                Notification.success({message: 'Project created.', delay: 8000});
+              }).error(function (data) {
+                Notification.error({message: data.message, title: 'Error ' + data.status, delay: 8000});
+              });
+            }
+          });
+        });
+      };
+
+      $scope.models = function (project_id, project_name) {
+        $location.path("/models/" + project_id + "/" + project_name);
+      };
+    }])
+
+  .controller('CreateProjectModalController', function ($scope, close) {
+    $scope.name = "";
+    $scope.close = function (result) {
+      close({
+        result: result,
+        name: $scope.name
+      }, 500);
     };
-}]);
+  });
--- app/views/workspaces/workspaces.js
+++ app/views/workspaces/workspaces.js
@@ -12,8 +12,8 @@ angular.module('artisStudio.workspaces', ['ngRoute'])
     });
   }])
 
-  .controller('WorkspacesCtrl', ['$scope', '$rootScope', 'DataFactory', '$location',
-    function ($scope, $rootScope, DataFactory, $location) {
+  .controller('WorkspacesCtrl', ['$scope', '$rootScope', 'DataFactory', '$location', 'ModalService', '$http', 'Notification',
+    function ($scope, $rootScope, DataFactory, $location, ModalService, $http, Notification) {
       $scope.workspaces = [];
 
       DataFactory.getData('workspaces').then(function (data) {
@@ -21,7 +21,26 @@ angular.module('artisStudio.workspaces', ['ngRoute'])
       });
 
       $scope.create = function () {
-        $location.path("workspace/-1");
+        ModalService.showModal({
+          templateUrl: 'createWorkspaceModal.html',
+          controller: "CreateWorkspaceModalController"
+        }).then(function (modal) {
+          modal.element.modal();
+          modal.close.then(function (result) {
+            if (result.result) {
+              $http.post($rootScope.urlBase + '/api/v1/workspace/' + $rootScope.user.id, {
+                name: result.name
+              }).success(function (data) {
+                DataFactory.getData('workspaces').then(function (data) {
+                  $scope.workspaces = data.data;
+                });
+                Notification.success({message: 'Workspace created.', delay: 8000});
+              }).error(function (data) {
+                Notification.error({message: data.message, title: 'Error ' + data.status, delay: 8000});
+              });
+            }
+          });
+        });
       };
 
       $scope.edit = function (workspace_id) {
@@ -31,4 +50,14 @@ angular.module('artisStudio.workspaces', ['ngRoute'])
       $scope.projects = function (workspace_id, workspace_name) {
         $location.path("projects/" + workspace_id + "/" + workspace_name);
       };
-    }]);
+    }])
+
+  .controller('CreateWorkspaceModalController', function ($scope, close) {
+    $scope.name = "";
+    $scope.close = function (result) {
+      close({
+        result: result,
+        name: $scope.name
+      }, 500);
+    };
+  });
